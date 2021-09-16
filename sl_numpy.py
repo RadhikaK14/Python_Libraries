c3567daa1b8931673161b70c36798494d022adf0
RadhikaK14 <62436185+RadhikaK14@users.noreply.github.com>
2021-09-16 15:00:33 +0100

    Initial commit

---
+++ sl_numpy.py
@@ -0,0 +1,25 @@
+import math as m
+# import numpy as np
+#
+# array = np.array([5,8,4,3])
+# print(array.mean())
+'''
+x = [5,8,4,3]
+array = 0
+sum = 0
+for num in x:
+    array = array + num * (x.index(num))
+    sum = sum + num
+mean = array/sum
+# mean = sum(x)/len(x)
+print(f'mean : {mean}')
+
+diff = 0
+for num in x:
+    diff = diff + ((num-mean)**2)
+variance = diff / len(x)
+print(f'Variance : {variance}')
+'''
+
+
+
